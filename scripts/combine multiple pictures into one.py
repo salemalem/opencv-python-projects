ee4608cfd614ea74a191951472fb54cbf9d03506
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2020-11-26 22:48:57 +0600

    resizing image and combine images added

---
+++ scripts/combine multiple pictures into one.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("../resources/small-sunflowers.jpg")
+
+# horizontally combined images
+imgHor = np.hstack((img, img))
+# vertically combined images
+imgVer = np.vstack((img, img))
+
+cv2.imshow("Horizantally combined images", imgHor)
+cv2.imshow("Vertically combined images", imgVer)
+
+cv2.waitKey(0) # put milliseconds as an argument 0 means infinite
---
+++ scripts/resizing image.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("../resources/black-friday.jpg")
+
+# Resizing image (Making it smaller) because original image is too big
+scale_percent = 5
+width = int(img.shape[1] * scale_percent / 100)
+height = int(img.shape[0] * scale_percent / 100)
+dimensions = (width, height)
+
+resizedSmallerImage = cv2.resize(img, dimensions, interpolation=cv2.INTER_AREA)
+
+# Showing an image
+cv2.imshow("Resized Smaller Image", resizedSmallerImage)
+
+cv2.waitKey(0)  # put milliseconds as an argument 0 means infinite
